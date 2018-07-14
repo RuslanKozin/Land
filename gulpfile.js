c161ee8b530eaa42af35bbfb214e7e4f8a00c2e7
RuslanKozin <ruslan3723@mail.ru>
2018-07-14 17:54:13 +0300

    add files

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+//npm i --save-dev gulp gulp-less gulp-sourcemaps gulp-autoprefixer gulp-concat gulp-if browser-sync gulp-clean-css
+var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCss = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var gulpIf = require('gulp-if');
+var browserSync = require('browser-sync').create();
+
+var config = {
+    paths: {    //Объект paths
+        less: './src/less/**/*.less',
+        html: './public/index.html'
+    },
+    output: {    //Объект output
+        cssName: 'bundle.min.css',
+        // cssStyle: './public/style',
+        path: './public'
+    },
+    isDevelop: false     //Объект isDevelop| Для финальной версии переключаем на false
+};
+
+gulp.task('less', function() {
+    return gulp.src(config.paths.less)
+        .pipe(gulpIf(config.isDevelop, sourcemaps.init()))
+        .pipe(less())
+        .pipe(concat(config.output.cssName))
+        .pipe(autoprefixer())
+        .pipe(gulpIf(!config.isDevelop, cleanCss()))
+        .pipe(gulpIf(config.isDevelop, sourcemaps.write()))
+        .pipe(gulp.dest(config.output.path))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: config.output.path
+        }
+    });
+
+    gulp.watch(config.paths.less, ['less']);
+    gulp.watch(config.paths.html).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['less', 'serve']);
